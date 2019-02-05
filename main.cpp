444f6fad0ca3e50e0562f82d25a3f6dd91f7990c
CG Bahk <cg.bahk@gmail.com>
2019-02-05 19:06:32 +0900

    Make notice to comments

--- main.cpp
+++ main.cpp
@@ -88,8 +88,8 @@ struct Mod
 /**
  * @brief generate from P[,x] to P[,2x]
  *
- * Both 'from' and 'to' are assumed to be have exactly MAX size.
  * It basically does convolution using NTT.
+ * @note Both 'from' and 'to' are assumed to be have exactly MAX size.
  * @param flen length of valid (non-zero) 'from' index
  */
 void times2(const Mod *from, Mod *to, int flen);
@@ -97,8 +97,8 @@ void times2(const Mod *from, Mod *to, int flen);
 /**
  * @brief generate from P[,x] to P[,x+1]
  *
- * Both 'from' and 'to' are assumed to be have exactly MAX size.
  * It basically implements recurrent relation.
+ * @note Both 'from' and 'to' are assumed to be have exactly MAX size.
  * @param flen length of valid (non-zero) 'from' index
  * @param used list of used digits, e.g. 4 0 2
  */
